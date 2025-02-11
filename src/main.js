12eb6d3364ef11e009caf8176b0699a8c9473802
Eran <eran.hasarali.2001@gmail.com>
2025-02-11 22:17:38 +0530

    remove all elements

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
 import "./style.css";
-
-let count = 0;
-
-const IncCount = () => {
-  count++;
-  document.querySelector(".count").textContent = count;
-};
-
-const DicCount = () => {
-  count--;
-  document.querySelector(".count").textContent = count;
-};
-
-window.IncCount = IncCount;
-window.DicCount = DicCount;
