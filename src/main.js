b9ff345a39f20717bd5d3ea0b28c56420348f9d9
Eran <eran.hasarali.2001@gmail.com>
2025-02-11 22:21:19 +0530

    create simple js class

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import "./style.css";
 
-const name = "John Doe";
-console.log(`Hello, ${name}!`);
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    alert(`Hello, I'm ${this.name}, and I'm ${this.age} years old.`);
+  }
+}
+
+const p1 = new Person("Era", 24);
+console.log(p1.greet());
