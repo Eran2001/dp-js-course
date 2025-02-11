54012e544575ad181c240109b0bc3a678eabd5e2
Eran <eran.hasarali.2001@gmail.com>
2025-02-11 19:15:52 +0530

    add list of div using DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import "./style.css";
+
+const myLst = ["Apple", "Orange", "Banana", "Grapes"];
+
+const myDiv = document.createElement("div");
+myDiv.innerHTML = myLst.join(", ");
+document.body.appendChild(myDiv);
