6e85dc572d27c6e5f918fc0f738a02c0b8572470
Eran <eran.hasarali.2001@gmail.com>
2025-02-12 10:08:17 +0530

    delete some codes

--- src/main.js
+++ src/main.js
@@ -2,14 +2,3 @@ import "./style.css";
 
 const heading = document.querySelector(".heading");
 heading.style.color = "#ff1";
-
-const h2 = document.createElement("h2");
-h2.textContent = "Eran Hasareli";
-document.body.appendChild(h2);
-
-h2.id = "myH2";
-h2.className = "myH2";
-console.log(h2);
-
-const all = document.querySelector(".heading");
-console.log(all);
