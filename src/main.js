3de25228cf97784a458e21a84f97d127b7eebde7
Eran <eran.hasarali.2001@gmail.com>
2025-02-12 15:58:31 +0530

    resets some elements and styling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
 import "./style.css";
-
-const heading = document.querySelector(".heading");
-heading.style.color = "#ff1";
-
-heading.setAttribute("draggable", "true");
-
-heading.style.marginTop = "4rem";
-
-heading.classList.add("cssClass");
