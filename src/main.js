595f36da69241d1e544c2b69ecf114aef826fd22
Eran <eran.hasarali.2001@gmail.com>
2025-02-12 08:56:41 +0530

    add a horizontal scroll bar using js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@ let myList = {
 };
 
 console.log(myList);
+
+window.addEventListener("scroll", function () {
+  const scrollBar = document.querySelector(".scroll");
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+
+  scrollBar.style.width = scrollPercent + "%";
+});
