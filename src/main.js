5936e73dccc5948acff0670ba4c5535c13ca0a0d
Eran <eran.hasarali.2001@gmail.com>
2025-02-12 09:19:59 +0530

    create h2 element using DOM

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import "./style.css";
 
 const heading = document.querySelector(".heading");
 heading.style.color = "#ff1";
+
+const h2 = document.createElement("h2");
+h2.textContent = "Eran Hasareli";
+document.body.appendChild(h2);
