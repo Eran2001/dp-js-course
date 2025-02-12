1d810cdb9514ef5c7b4793420a238b7c827061e3
Eran <eran.hasarali.2001@gmail.com>
2025-02-12 08:57:30 +0530

    remove scrool bar

--- src/main.js
+++ src/main.js
@@ -6,12 +6,3 @@ let myList = {
 };
 
 console.log(myList);
-
-window.addEventListener("scroll", function () {
-  const scrollBar = document.querySelector(".scroll");
-  const scrollTop = window.scrollY;
-  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercent = (scrollTop / docHeight) * 100;
-
-  scrollBar.style.width = scrollPercent + "%";
-});
