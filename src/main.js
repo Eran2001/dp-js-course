13034fe9a79ca0837c47069f907e22caa4e5a306
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 16:32:12 +0530

    resets all again

--- src/main.js
+++ src/main.js
@@ -1,19 +1,1 @@
 import "./style.css";
-
-let btnDark = document.getElementById("dark");
-let btnLight = document.getElementById("light");
-
-const addDarkClassFunc = () => {
-  let myClass = document.querySelector("html");
-  myClass.classList.add("myDarkClass");
-  myClass.classList.remove("myLightClass");
-};
-
-const addLightClassFunc = () => {
-  let myClass = document.querySelector("html");
-  myClass.classList.add("myLightClass");
-  myClass.classList.remove("myDarkClass");
-};
-
-btnDark.addEventListener("click", addDarkClassFunc);
-btnLight.addEventListener("click", addLightClassFunc);
