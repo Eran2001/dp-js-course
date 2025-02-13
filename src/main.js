2673db73db6ecb7b1e1a176301f83f927b101b54
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 21:19:23 +0530

    add class method different way

--- src/main.js
+++ src/main.js
@@ -4,14 +4,9 @@ class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
-
-    function greet() {
-      console.log(`Hello, ${name}`);
-    }
-
-    greet();
+    this.greet = () => console.log(`Hello, ${name}`);
   }
 }
 
 const person1 = new Person("Era", 24);
-person1.greet;
+person1.greet();
