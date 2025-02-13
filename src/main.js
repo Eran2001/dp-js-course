de1b7a24035e9607ee58cef6081f954af04f8472
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 17:43:56 +0530

    clear all

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
 import "./style.css";
-
-const btn = document.querySelector("button");
-const h2 = document.querySelector("h2");
-
-const myFunc = () => {
-  setTimeout(() => {
-    h2.innerHTML = "Clicked";
-  }, 2000);
-};
-
-window.myFunc = myFunc;
