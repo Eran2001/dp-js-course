92567334f0b03096ce4b1ae44ce52838a96b2b17
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 16:12:03 +0530

    add dark and light mode function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import "./style.css";
 
-let btn = document.querySelector("button");
-btn.addEventListener("click", () => alert("Clicked!"));
+let btnDark = document.getElementById("dark");
+let btnLight = document.getElementById("light");
+
+const addDarkClassFunc = () => {
+  let myClass = document.querySelector("html");
+  myClass.classList.add("myDarkClass");
+};
+
+const addLightClassFunc = () => {
+  let myClass = document.querySelector("html");
+  myClass.classList.add("myLightClass");
+};
+
+btnDark.addEventListener("click", addDarkClassFunc);
+btnLight.addEventListener("click", addLightClassFunc);
