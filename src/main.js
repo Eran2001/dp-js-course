5c317807d7024628287c420118a6829bd25b7e22
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 19:01:07 +0530

    clear all

--- src/main.js
+++ src/main.js
@@ -1,18 +1,1 @@
 import "./style.css";
-
-let myHeading = document.createElement("h1");
-
-const greet = (name) => {
-  myHeading.textContent = `Hello, ${name}`;
-  document.body.appendChild(myHeading);
-};
-
-const userInput = (callback) => {
-  let name = prompt("Enter your name: ");
-  if (!name) {
-    alert("Please enter a valid name!");
-  }
-  callback((name = "User"));
-};
-
-userInput(greet);
