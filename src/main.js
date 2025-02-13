7a8eb9849f14feff12721e0a3e7ddee7b3c2d37c
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 16:15:26 +0530

    add more methods

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ let btnLight = document.getElementById("light");
 const addDarkClassFunc = () => {
   let myClass = document.querySelector("html");
   myClass.classList.add("myDarkClass");
+  myClass.classList.remove("myLightClass");
 };
 
 const addLightClassFunc = () => {
   let myClass = document.querySelector("html");
   myClass.classList.add("myLightClass");
+  myClass.classList.remove("myDarkClass");
 };
 
 btnDark.addEventListener("click", addDarkClassFunc);
