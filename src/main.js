01b8ba1c19ec19443e32c558342e639689a4c639
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 15:49:36 +0530

    add event listener for the button

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
 import "./style.css";
 
-const showAlert = () => {
-  alert("This is a alert!");
-};
-
-const showPrompt = () => {
-  let name = prompt("Enter your name: ");
-  let nameHeading = document.createElement("h1");
-  nameHeading.textContent = name;
-  document.body.appendChild(nameHeading);
-};
-
-const showConfirm = () => {
-  confirm("Are you sure?");
-};
-
-window.showAlert = showAlert;
-window.showPrompt = showPrompt;
-window.showConfirm = showConfirm;
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  alert("Clicked!");
+});
