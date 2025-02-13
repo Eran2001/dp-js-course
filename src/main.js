2e6c23dcb1cb45af8886c6a71efd9f59f80b94cc
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 19:08:35 +0530

    test Promises

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const number = 0;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (!number) {
+    reject("Please enter a valid number");
+  } else {
+    resolve(number);
+  }
+});
+
+myPromise
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((data) => {
+    console.log(data);
+  });
