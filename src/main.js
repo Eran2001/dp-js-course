1bfeecb61e43f31739fa7fe38aad11ba1cc26025
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 21:32:34 +0530

    modify something

--- src/main.js
+++ src/main.js
@@ -9,5 +9,16 @@ class Person {
 }
 
 const person1 = new Person("Era", 24);
+const person2 = new Person("Hasa", 23);
 person1.greet();
-console.log(person1.__proto__);
+
+person2.sayHello = () => {
+  console.log("Hello person 2");
+};
+
+Person.prototype.sayHello = function () {
+  console.log(`Hello, ${this.name}`);
+};
+
+person2.sayHello();
+person1.sayHello();
