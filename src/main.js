1043b3e4b90cf2dfc0bb6a9a3a5e0fd7ed38fdcb
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 17:55:13 +0530

    use fetch api to get data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const response = fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => {
+    let myList = document.createElement("li");
+    myList.textContent = JSON.stringify(json);
+
+    document.body.appendChild(myList);
+  })
+  .catch((error) => {
+    console.error("Error Fetching data", error);
+  });
