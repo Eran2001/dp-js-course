7cfceb4c96f50946333d75802e55aa178d49508b
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 17:01:08 +0530

    test setTimeout function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const btn = document.querySelector("button");
+const h2 = document.querySelector("h2");
+
+const myFunc = () => {
+  setTimeout(() => {
+    h2.innerHTML = "Clicked";
+  }, 2000);
+};
+
+window.myFunc = myFunc;
