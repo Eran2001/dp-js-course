a88d8ce780d2048f418a0d2445829efa2008e633
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 18:55:46 +0530

    remove fetch api

--- src/main.js
+++ src/main.js
@@ -1,34 +1,9 @@
 import "./style.css";
 
-let myList = document.querySelector(".myList");
-
-const getData = async () => {
-  const url = "https://jsonplaceholder.typicode.com/posts";
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response Status: ${response.status}`);
-    }
-
-    const json = await response.json();
-
-    json.forEach((post) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = post.title;
-      myList.appendChild(listItem);
-    });
-  } catch (error) {
-    console.error("Error fetching data", error);
-  }
-};
-
-window.getData = getData;
-
 let myHeading = document.createElement("h1");
 
 const greet = (name) => {
-  myHeading.textContent = name;
+  myHeading.textContent = `Hello, ${name}`;
   document.body.appendChild(myHeading);
 };
 
