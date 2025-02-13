79f679d7dc3584e7c28716a9cea42ba57d590a1a
Eran <eran.hasarali.2001@gmail.com>
2025-02-13 19:52:41 +0530

    add class method

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
+
+    function greet() {
+      console.log(`Hello, ${name}`);
+    }
+
+    greet();
   }
 }
 
 const person1 = new Person("Era", 24);
-console.log(person1);
+person1.greet;
