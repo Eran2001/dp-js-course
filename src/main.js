b36e1761df1a2b9f60fd4ffff2ed806b0955e8f8
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 08:02:57 +0530

    add event listener when button is cliked

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import "./style.css";
+
+let color = document.getElementById("color");
+let heading = document.querySelector(".myHead");
+
+color.addEventListener("click", () => {
+  heading.classList.add("color");
+});
