2126869ba0f76a2f96f3b7738872e58052f77c39
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 14:34:08 +0530

    add straight line

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(50, 100, 200, 100);
+ctx.fillRect(300, 400, 200, 100);
+
+ctx.fillStyle = "#FF1";
+ctx.fillRect(0, 0, 100, 100);
 
 ctx.fillStyle = "#0000FF";
 ctx.beginPath();
 ctx.arc(300, 150, 50, 0, 2 * Math.PI);
 ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(500, 0);
+ctx.lineTo(0, 500);
+
+ctx.strokeStyle = "#000";
+ctx.stroke();
