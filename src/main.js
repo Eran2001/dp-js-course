025ee3834f12d2f164b945848fa634b6ab79bd9c
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 12:25:48 +0530

    done

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ const total = () => {
   let c = prompt("c: "); // big
 
   if (!a) {
-    alert("You have to give atleast 2 numbers");
+    const total = `${c * c - b * b}`;
+    console.log("c2 = a2 + b2");
+    console.log("a2 = c2 - b2");
+    console.log(`a2 = ${c * c} - ${b * b}`);
+    console.log(`a2 = ${total}`);
+    console.log(Math.sqrt(total));
   }
 };
 
