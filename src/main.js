2546fc9e6e622821719a03d039e3831f3d83322a
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 12:18:09 +0530

    prompt user for inputs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import "./style.css";
-import add from "./math";
 
-console.log(add(1, 3));
+const total = () => {
+  let a = prompt("a: "); // base
+  let b = prompt("b: "); // 90
+  let c = prompt("c: "); // big
+
+  if (!a) {
+    alert("You have to give atleast 2 numbers");
+  }
+};
+
+total();
