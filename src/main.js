7bb12d33b110bbebbb9d8a59e732729cd3365d20
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 08:07:34 +0530

    done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import "./style.css";
 
-let color = document.getElementById("color");
 let heading = document.querySelector(".myHead");
 
+let color = document.getElementById("color");
+let animate = document.getElementById("animate");
+let hidden = document.getElementById("hidden");
+
+// change color
 color.addEventListener("click", () => {
-  heading.classList.add("color");
+  heading.classList.toggle("color");
+});
+
+// add animation
+animate.addEventListener("click", () => {
+  heading.classList.toggle("animate");
+});
+
+// hidden
+hidden.addEventListener("click", () => {
+  heading.classList.toggle("hidden");
 });
