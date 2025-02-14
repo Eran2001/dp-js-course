aff3e7edeefd9b33da183761079e52d6a6ca51e4
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 07:49:42 +0530

    add some buttons and stylings

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
 import "./style.css";
-
-const students = [
-  { name: "Era", score: 75 },
-  { name: "Hasa", score: 80 },
-  { name: "Neo", score: 78 },
-];
-
-const topStudents = students.filter((student) => {
-  return student.score > 75;
-});
-
-console.log(topStudents);
