96224c1d911271fdb48575e7859cf8263ee20185
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 12:29:32 +0530

    add some lines

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ const total = () => {
     console.log("a2 = c2 - b2");
     console.log(`a2 = ${c * c} - ${b * b}`);
     console.log(`a2 = ${total}`);
-    console.log(Math.sqrt(total));
+    let newTotal = Math.sqrt(total);
+    console.log(`a = ${newTotal}`);
   }
 };
 
