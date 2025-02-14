17a7b9a5ffd1a49e49134ac0bed447c558ea343f
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 20:10:18 +0530

    add eventlistener for change color

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import "./style.css";
 
 const value = document.querySelector("#value");
 const box = document.querySelector(".box");
+const color = document.querySelector("#clr");
 
 value.addEventListener("change", () => {
   box.style.width = `${value.value}px`;
+});
+
+color.addEventListener("change", () => {
+  box.style.backgroundColor = color.value;
 });
