42401b927042a55e7f7b41cddc6bac2071773632
Eran <eran.hasarali.2001@gmail.com>
2025-02-14 10:14:31 +0530

    add another way

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import "./style.css";
 
 const btn1 = document.querySelector("button");
 const btn2 = document.getElementById("btn");
-const btn3 = document.getElementsByClassName("btn");
-const btn4 = document.getElementsByName("button");
+const btn3 = document.getElementsByClassName("btn")[0];
+const btn4 = document.querySelector(".btn");
 
-console.log(btn1);
-console.log(btn2);
-console.log(btn3);
 console.log(btn4);
