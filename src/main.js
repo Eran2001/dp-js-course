da9ae891ac290250ce543cbeb4d3266dd6b7eb62
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 12:02:52 +0530

    add simple qr for visit google

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+// new QRCode(document.getElementById("qr"), "http://www.google.com");
+
+var qr = new QRCode("qr", {
+  text: "http://jindo.dev.naver.com/collie",
+  width: 200,
+  height: 200,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H,
+});
