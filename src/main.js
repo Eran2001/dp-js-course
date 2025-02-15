e16eb550727f0a20ac2392aaf2e0df414bb66f5d
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 12:21:08 +0530

    test window object

--- src/main.js
+++ src/main.js
@@ -1,26 +1,5 @@
 import "./style.css";
-import QRCode from "qrcode";
 
-var canvas = document.getElementById("canvas");
+console.log(window);
 
-QRCode.toCanvas(canvas, "sample text", function (error) {
-  if (error) console.error(error);
-  console.log("success!");
-});
-
-QRCode.toDataURL("I am a pony!")
-  .then((url) => {
-    console.log(url);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-// With async/await
-const generateQR = async (text) => {
-  try {
-    console.log(await QRCode.toDataURL(text));
-  } catch (err) {
-    console.error(err);
-  }
-};
+window.alert("Test");
