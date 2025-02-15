ca79590e3d772e8d5209febad005899464eaf72c
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 09:48:50 +0530

    modify some codes

--- src/main.js
+++ src/main.js
@@ -2,17 +2,28 @@ import "./style.css";
 
 const name = document.getElementById("name");
 const login = document.getElementById("login");
+const h2 = document.getElementById("h2");
+
+// function to display user name dynamically
+function updateName() {
+  const storedName = localStorage.getItem("user_name");
+  if (storedName) {
+    h2.textContent = `Welcome, ${storedName}!`;
+  }
+}
+
+//function to remove name dynamically
 
 login.addEventListener("click", () => {
   localStorage.setItem("user_name", name.value);
   console.log("user name created");
+  updateName();
 });
-
-const h2 = document.getElementById("h2");
-h2.textContent = localStorage.getItem("user_name");
 
 const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
   localStorage.removeItem("user_name");
   console.log("user name removed");
 });
+
+updateName();
