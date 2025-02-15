c66e9fd1bde1c2d223e52e6a3dba2f974df32275
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 12:13:05 +0530

    add async fntion

--- src/main.js
+++ src/main.js
@@ -8,10 +8,19 @@ QRCode.toCanvas(canvas, "sample text", function (error) {
   console.log("success!");
 });
 
-QRCode.toDataURL("I am a pony!", function (err, url) {
-  console.log(url);
-});
+QRCode.toDataURL("I am a pony!")
+  .then((url) => {
+    console.log(url);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-QRCode.toString("I am a pony!", { type: "terminal" }, function (err, url) {
-  console.log(url);
-});
+// With async/await
+const generateQR = async (text) => {
+  try {
+    console.log(await QRCode.toDataURL(text));
+  } catch (err) {
+    console.error(err);
+  }
+};
